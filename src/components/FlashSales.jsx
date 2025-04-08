085cac14812dc0687fa592589a76ce21b27d769e
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-08 16:53:37 +0000

    Update

--- src/components/FlashSales.jsx
+++ src/components/FlashSales.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { getProducts } from '../api/Products';
-import '../styles/FlashSales.css';
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../api/Products";
+import "../styles/FlashSales.css";
+import { motion } from 'framer-motion';
 
 // const products = [
 //   { id: 1, name: "HAVIT HV-G92 Gamepad", price: 120, discount: 40, img: "/images/gamepad.jpg" },
@@ -10,25 +11,35 @@ import '../styles/FlashSales.css';
 
 export default function FlashSales() {
   const [Products, setProducts] = useState([]);
-  
-    useEffect(()=>{
-      const getData = async () => {
-        const data = await getProducts();
-        console.log("PRODUCTS DATA === ", data);
-        setProducts(data.data);
-      }
-      getData();
-    }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getProducts();
+      console.log("PRODUCTS DATA === ", data);
+      setProducts(data.data);
+    };
+    getData();
+  }, []);
 
   return (
     <>
       <h2 className="">🔥 Flash Sales</h2>
       <div className="flashSalesContainer">
-        {Products.map(p => (
+        {Products.map((p) => (
           <div key={p.id} className="productContainer">
-            <img src={p.image_url} alt={p.name} className="" />
+            {/* <img src={p.image_url} alt={p.name} className="" /> */}
+            <motion.img
+              src={p.image_url}
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              whileHover={{ scale: 1.20, rotate: 2 }}
+              transition={{ duration: 0.5 }}
+            />
             <h3 className="mt-2">{p.name}</h3>
-            <p className="text-red-500">${(p.price * (1 - p.discount_price / 100)).toFixed(2)} <span className="line-through text-gray-400">${p.price}</span></p>
+            <p className="text-red-500">
+              ${(p.price * (1 - p.discount_price / 100)).toFixed(2)}{" "}
+              <span className="line-through text-gray-400">${p.price}</span>
+            </p>
           </div>
         ))}
       </div>
