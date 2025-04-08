e2b0f95bf626c48016afefc244c963c1b08365ad
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-08 14:51:38 +0000

    Update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
         <h2 className="text-2xl mb-4">Up to 10% off Voucher</h2>
         <button className="bg-white text-black px-4 py-2 rounded">Shop Now</button>
       </div>
-      <img src="/images/hero-phone.png" alt="iPhone" className="w-60" />
+      <img src="http://localhost:5000/images/uploads/website_banner.jpg" alt="iPhone" className="banner_image" />
     </section>
   );
 }
