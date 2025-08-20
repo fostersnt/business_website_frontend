c4c486cf1472dbaee724db0f091b9e577807814e
Foster Amponsah <foster.asante@gwosevo.com>
2025-08-20 11:01:27 +0000

    Update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styles/Logo.css';
 
 const Logo = () => {
-  const styles = {
-    logo: {
-      fontSize: "32px",
-      fontWeight: "bold",
-      backgroundColor: "#fff",
-      padding: "5px",
-      border: "1px solid #ff5722",
-      borderRadius: "30px"
-      //   fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
-      //   display: "inline-block",
-    },
-    dictum: {
-      color: "#0d47a1", // deep blue
-    },
-    logic: {
-      color: "#ff5722", // vibrant orange
-    },
-  };
+  // const styles = {
+  //   logo: {
+  //     fontSize: "32px",
+  //     fontWeight: "bold",
+  //     backgroundColor: "#fff",
+  //     padding: "5px",
+  //     border: "1px solid #ff5722",
+  //     borderRadius: "30px"
+  //   },
+  //   dictum: {
+  //     color: "#0d47a1",
+  //   },
+  //   logic: {
+  //     color: "#ff5722",
+  //   },
+  // };
+
+  // return (
+  //   <Link to="/">
+  //     <div style={styles.logo}>
+  //       <span style={styles.dictum}>Dictum</span>
+  //       <span style={styles.logic}>Logic</span>
+  //     </div>
+  //   </Link>
+  // );
 
   return (
     <Link to="/">
-      <div style={styles.logo}>
-        <span style={styles.dictum}>Dictum</span>
-        <span style={styles.logic}>Logic</span>
+      <div className="logo">
+        <span className="dictum">Dictum</span>
+        <span className="logic">Logic</span>
       </div>
     </Link>
   );
