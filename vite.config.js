4d80dbb7df4f1223df61f821c43648b1f7f02674
Foster Amponsah <foster.asante@gwosevo.com>
2025-08-20 08:55:04 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/business_website_frontend/',  // <-- add this line
+  base: '/',
   plugins: [react()],
 })
